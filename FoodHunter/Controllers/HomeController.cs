3beff32152e4089e4041a1439e4c6ea57859fc86
beelzebub <beelzebubm@outlook.com>
2019-10-14 03:09:04 +1100

    implenment email, upload file and rich text editor

--- FoodHunter/Controllers/HomeController.cs
+++ FoodHunter/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FoodHunter.Utils;
+using FoodHunter.Models;
+using System.IO;
 
 namespace FoodHunter.Controllers
 {
@@ -11,6 +14,8 @@ namespace FoodHunter.Controllers
     {
         public ActionResult Index()
         {
+            EmailSender es = new EmailSender();
+
             return View();
         }
 
@@ -28,6 +33,72 @@ namespace FoodHunter.Controllers
             return View();
         }
 
-       
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult Send_Email(SendEmailViewModel model, HttpPostedFileBase postedFile)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+
+                    string imagePath = "";
+                    if (postedFile != null)
+                    {
+                        var myUniqueFileName = string.Format(@"{0}", Guid.NewGuid());
+                        imagePath = myUniqueFileName;
+                        //TryValidateModel(image);
+
+                        string serverPath = Server.MapPath("~/Uploads/");
+                        string fileExtension = Path.GetExtension(postedFile.FileName);
+                        string filePath = imagePath + fileExtension;
+                        imagePath = filePath;
+                        try
+                        {
+                            postedFile.SaveAs(serverPath + imagePath);
+                        }
+                        catch
+                        {
+                            ViewBag.Result = "file save failed.";
+                            return View();
+                        }
+                    }
+                    
+                        EmailSender es = new EmailSender();
+                        es.Send(toEmail, subject, contents, imagePath);
+
+                        ViewBag.Result = "Email has been send.";
+
+                        ModelState.Clear();
+
+                        return View(new SendEmailViewModel());
+                   
+                }
+                catch
+                {
+                    ViewBag.Result = "email not been sended";
+                    return View();
+                }
+               
+            }
+            else
+            {
+                ViewBag.Result = "Email model has error.";
+                return View();
+            }
+
+            return View();
+        }
+
+
+
     }
+
 }
---
+++ FoodHunter/Utils/EmailSender.cs
@@ -0,0 +1,47 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FoodHunter.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents, String filename)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("noreply@localhost.com", "Food Hunter");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            //string serverPath = System.Web.HttpContext.Current.Server.MapPath("~/Uploads/");
+            // Attachment attachment = new Attachment();
+            // attachment.Filename = serverPath;
+            //msg.Attachments.Add(attachment);
+            if (!filename.Equals(""))
+            {
+                byte[] bytes = File.ReadAllBytes(System.Web.HttpContext.Current.Server.MapPath(@"~/Uploads/" + filename));
+                string fileContentsAsBase64 = Convert.ToBase64String(bytes);
+
+                var attachment = new Attachment
+                {
+                    Filename = filename,
+                    Type = "application/jpeg",
+                    Content = fileContentsAsBase64
+                };
+                msg.AddAttachment(attachment);
+            }
+           // msg.Attachments.Add(attachment);
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
